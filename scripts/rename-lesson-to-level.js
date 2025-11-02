571d1d129b82701859dd22fa8de3cbc216801777
doankyvi298-eng <doankyvi298@gmail.com>
2025-11-02 17:45:47 +0800

    Delete scripts directory

--- scripts/rename-lesson-to-level.js
+++
@@ -1,6 +0,0 @@
-﻿const fs=require('fs'); const p=require('path');
-const file=p.resolve(__dirname,'..','src','lib','data.js');
-let s=fs.readFileSync(file,'utf8');
-s=s.replace(/'第'\+i\+'课'/g, "'第'+i+'关'");
-fs.writeFileSync(file,s,'utf8');
-console.log('data.js names -> 关');
--- scripts/update-wintext.js
+++
@@ -1,7 +0,0 @@
-﻿const fs = require('fs');
-const p = require('path');
-const file = p.resolve(__dirname, '..', 'src', 'game.js');
-let js = fs.readFileSync(file, 'utf8');
-js = js.replace(/本课完成！/g, '本关完成！');
-fs.writeFileSync(file, js, 'utf8');
-console.log('win text updated');
